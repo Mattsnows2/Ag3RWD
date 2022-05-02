b0749e00e866e53923c64b2ce24b5f890d10bfeb
Mattsnows2 <matthias.belot54@gmail.com>
2022-05-02 10:24:08 +0200

    apple

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-$(".apple").on("click", function(){
+$("#apple1").on("click", function(){
 
-    $(".apple").animate({right: "-500", top:"500"}, "slow")
+    $("#apple1").animate({left: "-550", top:"420", degree:360}, "slow") 
+  
+   
+});
+
+$("#apple2").on("click", function(){
+
+    $("#apple2").animate({left: "-400", top:"460"}, "slow") //animation of the apples
+});
+
+$("#apple3").on("click", function(){
+
+    $("#apple3").animate({left: "-950", top:"520"}, "slow") //animation of the apples
 });
 //MAKE THE MAGIC HAPPEN
 $(document).mousemove(function(e) {
@@ -14,3 +26,11 @@ $(document).mousemove(function(e) {
 $('#butterfly').mouseenter(function() {
     $(this).animate({left: Math.random() * window.outerWidth - 100, top: Math.random() * window.outerHeight - 150},"slow");
 });
+
+$(document).ready(function() {
+   
+
+        $("#backgroundImage").attr("src",'images/night.jpg').delay(2000);
+    });
+ 
+
