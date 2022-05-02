dfd945755ccd797d9143b533c4cc142fb42de62d
ericmartihaynes <ericmartihaynes@gmail.com>
2022-05-02 09:12:14 +0200

    net and butterfly started

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 //MAKE THE MAGIC HAPPEN
+$(document).mousemove(function(e) {
+    $('#net').offset({
+        left: e.pageX -60,
+        top: e.pageY -100
+    });
+});
+
+
+$('#butterfly').mouseenter(function() {
+    $(this).animate({left: Math.random() * window.outerWidth - 100, top: Math.random() * window.outerHeight - 150},"slow");
+});
